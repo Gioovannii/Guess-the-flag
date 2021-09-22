e3c6ac716ba322382b7df6ea9c5375e6afde59f0
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-22 10:31:30 +0800

    Initial Commit

---
+++ Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_flagApp.swift
+//  Guess the flag
+//
+//  Created by Giovanni Gaffé on 2021/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
